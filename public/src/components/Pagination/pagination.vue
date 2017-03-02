53de5782c83e58464ea00b717acf4207a222c5f3
taoxin <taoxin167@gmail.com>
2017-03-02 18:16:31 +0800

    调整pagination组件

--- public/src/components/Pagination/pagination.vue
+++ public/src/components/Pagination/pagination.vue
@@ -2,6 +2,7 @@
 	<div>
 		<div class="paginations" >
 			<span>共{{totalRows}}条记录/每页{{perPage}}记录</span>
+			<button type="button" class="btn btn-pagination-up" v-bind:disabled="currentPage == 1 " v-on:click.prevent="currentPage = 1"><span aria-hidden="true">首页</span></button>
 			<button type="button" class="btn btn-pagination-up" v-bind:disabled="currentPage == 1 " v-on:click.prevent="(currentPage == 1) ? return : currentPage--"><span aria-hidden="true">上一页</span></button>
 			<button type="button" class="btn  btn-pagination {{ currentPage == 1 ?  'active' : '' }}" v-on:click.prevent="currentPage = 1" v-show="showPrev">1</button>
 			<span class="btn btn-pagination" v-show="showPrev">...</span>
@@ -13,6 +14,7 @@
 		<span class="btn btn-pagination" v-show="showNext">...</span>
 		<button type="button" class="btn btn-pagination {{ numberOfPages == currentPage ? 'active' : '' }}" v-show="showNext" v-on:click.prevent="currentPage = numberOfPages">{{numberOfPages}}</button>
 		<button type="button" class="btn btn-pagination-up" v-bind:disabled="currentPage == numberOfPages" v-on:click.prevent="(currentPage == numberOfPages) ? return : currentPage++"><span aria-hidden="true">下一页</span></button>
+		<button type="button" class="btn btn-pagination-up" v-bind:disabled="currentPage == numberOfPages" v-on:click.prevent="currentPage =numberOfPages"><span aria-hidden="true">末页</span></button>
 	</div>
 </template>
 <script>
